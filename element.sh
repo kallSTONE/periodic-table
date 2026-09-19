695e88b3d49786eb9ba336a238f2dd27ff0dfee4
Kalab Tadesse <kalupt9999@gmail.com>
2026-09-19 07:06:54 -0400

    feat: implement database query logic

--- element.sh
+++ element.sh
@@ -5,4 +5,14 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
+else
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    QUERY="e.atomic_number = $1"
+  elif [[ ${#1} -le 2 ]]
+  then
+    QUERY="e.symbol = '$1'"
+  else
+    QUERY="e.name = '$1'"
+  fi
 fi
