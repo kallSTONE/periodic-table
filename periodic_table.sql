4aaf5ec4cc581008b969d4437f742195745b4d5c
Kalab Tadesse <kalupt9999@gmail.com>
2026-09-19 07:31:28 -0400

    fix: remove trailing zeros from atomic mass

--- periodic_table.sql
+++ periodic_table.sql
@@ -132,14 +132,14 @@ INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
 -- Data for Name: properties; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.properties VALUES (1, 1.008000, -259.1, -252.9, 3);
-INSERT INTO public.properties VALUES (2, 4.002600, -272.2, -269, 3);
-INSERT INTO public.properties VALUES (3, 6.940000, 180.54, 1342, 1);
-INSERT INTO public.properties VALUES (4, 9.012200, 1287, 2470, 1);
-INSERT INTO public.properties VALUES (5, 10.810000, 2075, 4000, 2);
-INSERT INTO public.properties VALUES (6, 12.011000, 3550, 4027, 3);
-INSERT INTO public.properties VALUES (7, 14.007000, -210.1, -195.8, 3);
-INSERT INTO public.properties VALUES (8, 15.999000, -218, -183, 3);
+INSERT INTO public.properties VALUES (1, 1.008, -259.1, -252.9, 3);
+INSERT INTO public.properties VALUES (2, 4.0026, -272.2, -269, 3);
+INSERT INTO public.properties VALUES (3, 6.94, 180.54, 1342, 1);
+INSERT INTO public.properties VALUES (4, 9.0122, 1287, 2470, 1);
+INSERT INTO public.properties VALUES (5, 10.81, 2075, 4000, 2);
+INSERT INTO public.properties VALUES (6, 12.011, 3550, 4027, 3);
+INSERT INTO public.properties VALUES (7, 14.007, -210.1, -195.8, 3);
+INSERT INTO public.properties VALUES (8, 15.999, -218, -183, 3);
 INSERT INTO public.properties VALUES (9, 18.998, -220, -188.1, 3);
 INSERT INTO public.properties VALUES (10, 20.18, -248.6, -246.1, 3);
 
